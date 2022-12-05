4fb5064a2f325f7e45b3df7a089ec0eaf5dfb439
Zaqueu Luna <zaqueuluna@MacBook-Air-de-Zaqueu.local>
2022-12-05 16:02:10 -0300

    UPDATE

---
+++ js/carrinho.js
@@ -0,0 +1,134 @@
+const pizzaJson = [
+    {
+        id: 1,
+        name: 'Frapuccino Chocolate',
+        img: 'imagens/choc.png',
+        price: [10.00, 14.00, 18.00],
+        sizes: [
+          '300 ml',
+          '400 ml',
+          '500 ml'
+        ],
+        description: 'Café sem açucar, leite, calda de chocolate e chantilly'
+      },
+      {
+        id: 2,
+        name: 'Frapuccino Morango',
+        img: 'imagens/morango.png ',
+        price: [11.00, 15.00, 19.00],
+        sizes: [
+          '300 ml',
+          '400 ml',
+          '500 ml'
+        ],
+        description: 'Leite, morangos, essência de baunilha, calda de morango e chantilly'
+      },
+      {
+        id: 3,
+        name: 'Frapuccino de Flocos',
+        img: 'imagens/flocos.png',
+        price: [10.00, 13.00, 16.00],
+        sizes: [
+          '300 ml',
+          '400 ml',
+          '500 ml'
+        ],
+        description: 'Leite, café, calda de chocolate, gelo e chocolate em granulado'
+      },
+      {
+        id: 4,
+        name: 'Frapuccino de Doce de leite',
+        img: 'imagens/doce.png',
+        price: [12.00, 16.00, 20.00],
+        sizes: [
+          '300 ml',
+          '400 ml',
+          '500 ml'
+        ],
+        description: 'Leite, café, doce de leite, sorvete de creme e chantilly'
+      },
+      {
+        id: 5,
+        name: 'Croissant de Frango',
+        img: 'imagens/croissant.png',
+        price: [4.00, 6.00, 8.00],
+        sizes: [
+          '100 g',
+          '200 g ',
+          '250 g'
+        ],
+        description: 'Massa fofinha e folhada ,recheado com nosso delicioso frango cremoso com cream cheese'
+      },
+      {
+        id: 6,
+        name: 'Coxinha de Camarão',
+        img: 'imagens/coxinha.png',
+        price: [5.00, 7.00, 10.00],
+        sizes: [
+          '100 g',
+          '200 g',
+          '280 g'
+        ],
+        description: 'Massa de batata, recheada com nosso delicioso patê de camarão com mix de queijos'
+      },
+      {
+        id: 7,
+        name: 'Brownies',
+        img: 'imagens/brow.png',
+        price: [3.00, 6.00, 8.00],
+        sizes: [
+          '1 un',
+          '2 un',
+          '3 un'
+        ],
+        description: 'Nosso brownuie é feito com chocolate 70% cacau e gotas de chocolate'
+      },
+      {
+        id: 8,
+        name: 'Sanduiche Natural de Atum',
+        img: 'imagens/atum.png',
+        price: [7.00, 9.00, 11.00],
+        sizes: [
+          '150 g',
+          '220 g',
+          '340 g'
+        ],
+        description: 'Feito com ingredientes frescos e saborosos'
+      },
+      {
+        id: 9,
+        name: 'Quiche de Bacon  ',
+        img: 'imagens/Quiche.png',
+        price: [4.00, 5.00, 7.00],
+        sizes: [
+          '100 g',
+          '220 g',
+          '300 g'
+        ],
+        description: 'Recheado com Bacon, mix de queijos e tomate cereja'
+      },
+      {
+        id: 10,
+        name: 'Coca-cola',
+        img: 'imagens/coca.png',
+        price: [3.50, 4.00, 6.00],
+        sizes: [
+          '250 ml',
+          '350 ml',
+          '500 ml'
+        ],
+        description: 'Coca, gelo e limão '
+      },
+      {
+        id: 11,
+        name: 'Chocolate Quente ',
+        img: 'imagens/chocolatequente.png',
+        price: [4.00, 6.00, 7.00],
+        sizes: [
+          '120 ml',
+          '200 ml',
+          '250 ml'
+        ],
+        description: 'Leite fresco, calda de chocolate e chocolate 50% cacau'
+      }
+];
